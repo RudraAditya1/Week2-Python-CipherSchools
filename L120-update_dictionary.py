748f7191f83ae8cc5299c67200f554ad9b025962
Rudra Aditya <117707289+RudraAditya1@users.noreply.github.com>
2022-12-14 22:50:42 +0530

    Update L120-update_dictionary.py

--- L120-update_dictionary.py
+++ L120-update_dictionary.py
@@ -1,12 +1,12 @@
 # update method
 
 user_info={
-    'name':'harsh',
-    'age': 18,
-    'fav_movies': ['kabir singh'],
+    'name':'Aditya',
+    'age': 19,
+    'fav_movies': ['Hera Pheri'],
     'fav_tunes': ['awakening','fairy tale'],
 }
 
-more_info= {'state' : 'Bihar', 'hobbies' : ['coding','reading','playing_cricket']}
+more_info= {'state' : 'Bihar', 'hobbies' : ['coding','Watching Movies','playing_cricket']}
 user_info.update({})
 print(user_info)
